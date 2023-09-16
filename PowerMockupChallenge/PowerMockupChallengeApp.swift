d24b1b700fed29d0de9440dde060d7f713c0c147
Rachel Radford <rachelradford21@gmail.com>
2023-09-16 09:58:46 -0400

    Initial Commit

---
+++ PowerMockupChallenge/PowerMockupChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerMockupChallengeApp.swift
+//  PowerMockupChallenge
+//
+//  Created by Rachel Radford on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PowerMockupChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
